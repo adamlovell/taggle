aa9a619047e3149c07d8e8e1e215ec98a4fa6705
viperxiv <adam.lovell@adam-macpro.ff0000.com>
2014-05-02 17:23:41 -0700

    Change track to object

--- lib/sort.py
+++ lib/sort.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
-import os
-import sys 
-from distutils import dir_util
 from lib import tag
-from lib import move 
-
 
 def sort_track(track, keys): 
 
-    track_tags = tag.tag(track)
+    track_tags = tag.tag(track['source'])
+    track_dest_dir = str(track['starting_dir'])
+
+    for value in keys: 
+        print value  
+        print track_tags[value][0].strip() + '/'
+        print track_dest_dir
+
     track_dest_dir = str('./test_dest_dir/' + track_tags[keys[0]][0] +  '/' + track_tags[keys[1]][0] + '/' + track_tags[keys[2]][0] + '/')
 
     print (track_dest_dir)
-    move.move_track(track, track_dest_dir)
+    return track_dest_dir
+
 
--- taggler.py
+++ taggler.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python
 
 from lib import sort
+from lib import move
 
-track = 'test_source_dir/test.mp3'
-keys = ['genre', 'artist', 'album']
-
-sort.sort_track(track, keys)
-
-'''
-track_tags = tag.tag(new_track)
-track_dest_dir = str('./test_dest_dir/' + track_tags[keys[0]][0] +  '/' + track_tags[keys[1]][0] + '/' + track_tags[keys[2]][0] + '/')
-
-print (track_dest_dir)
-if not os.path.exists(track_dest_dir):
-   #os.mkdirs(track_dest_dir)
-    dir_util.mkpath(track_dest_dir)
-
-move.move_track(new_track, track_dest_dir)
-'''
+track = {}
+track['source'] = 'test_source_dir/test.mp3'
+track['starting_dir'] = './test_dest_dir/'
+keys = ['genre', 'artist', 'album'] 
+track['destination_dir'] = sort.sort_track(track, keys)
+move.move_track(track['source'], track['destination_dir'])
